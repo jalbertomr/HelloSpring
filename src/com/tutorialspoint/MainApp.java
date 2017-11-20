dd66126f0f5b48bf08f1bca58a254f3611976c48
jalbertomr <jalbertomr@yahoo.com>
2017-11-20 00:35:18 -0600

    context = new FileSystemXmlApplicationContext("C:\\Users\\Bext\\Desktop\\HelloSpring\\src\\Beans.xml");

--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -3,13 +3,14 @@ package com.tutorialspoint;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 public class MainApp {
     public static void main(String... args) {
-        XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("Beans.xml"));
+        FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("C:\\Users\\Bext\\Desktop\\HelloSpring\\src\\Beans.xml");
 
-        HelloWorld o = (HelloWorld) factory.getBean("helloWorld");
+        HelloWorld o = (HelloWorld) context.getBean("helloWorld");
         o.getMessage();
     }
 }
