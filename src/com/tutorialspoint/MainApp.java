37db71759744630c897ed12dc371ca37e14120d9
jalbertomr <jalbertomr@yahoo.com>
2017-11-20 00:29:45 -0600

    XmlBeanFactory(new ClassPathResource("Beans.xml");

--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -1,13 +1,15 @@
 package com.tutorialspoint;
 
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 public class MainApp {
     public static void main(String... args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("Beans.xml"));
 
-        HelloWorld o = (HelloWorld) context.getBean("helloWorld");
+        HelloWorld o = (HelloWorld) factory.getBean("helloWorld");
         o.getMessage();
     }
 }
