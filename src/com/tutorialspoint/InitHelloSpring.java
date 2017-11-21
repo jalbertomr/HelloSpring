3ecac469074e089d8d65ed6b674c396b0d407d08
jalbertomr <jalbertomr@yahoo.com>
2017-11-20 23:45:14 -0600

    Bean Post Processors, Ordered

--- src/com/tutorialspoint/InitHelloSpring.java
+++ src/com/tutorialspoint/InitHelloSpring.java
@@ -4,17 +4,21 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.core.Ordered;
 
-public class InitHelloSpring implements BeanPostProcessor{
+public class InitHelloSpring implements BeanPostProcessor, Ordered{
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("BeforeInitialization: " + beanName);
+        System.out.println(this.getClass().getSimpleName() + ": BeforeInitialization: " + beanName);
         return bean;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("AfterInitialization: " + beanName);
+        System.out.println(this.getClass().getSimpleName() + ": AfterInitialization: " + beanName);
         return bean;
     }
 
+    @Override
+    public int getOrder() {
+        return 1;
+    }
 }
---
+++ src/com/tutorialspoint/InitHelloSpring2.java
@@ -0,0 +1,24 @@
+package com.tutorialspoint;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.core.Ordered;
+
+public class InitHelloSpring2 implements BeanPostProcessor, Ordered {
+    @Override
+    public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
+        System.out.println(this.getClass().getSimpleName() + ": BeforeInitialization: " + s);
+        return o;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object o, String s) throws BeansException {
+        System.out.println(this.getClass().getSimpleName() + ": AfterInitialization: " + s);
+        return o;
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
