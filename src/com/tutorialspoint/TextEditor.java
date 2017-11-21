ca24a9d92f65aa6f3dd52b6d33faa3999e9622b4
jalbertomr <jalbertomr@yahoo.com>
2017-11-21 17:09:17 -0600

    Beans Autowiring 'byName'

--- src/com/tutorialspoint/TextEditor.java
+++ src/com/tutorialspoint/TextEditor.java
@@ -2,6 +2,7 @@ package com.tutorialspoint;
 
 public class TextEditor {
     private SpellChecker spellChecker;
+    private String name;
 
     public SpellChecker getSpellChecker() {
         return spellChecker;
@@ -10,6 +11,14 @@ public class TextEditor {
     public void setSpellChecker(SpellChecker spellChecker) {
         System.out.println("TextEditor: Dentro de setSpellChecker");
         this.spellChecker = spellChecker;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void spellCheck() {
