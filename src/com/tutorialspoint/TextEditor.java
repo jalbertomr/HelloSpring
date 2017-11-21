97125aa0f2d14183aaf21cfab67c37895a5fd211
jalbertomr <jalbertomr@yahoo.com>
2017-11-21 17:52:24 -0600

    Bean Autowire by Constructor SIN AUTOWIRE

--- src/com/tutorialspoint/TextEditor.java
+++ src/com/tutorialspoint/TextEditor.java
@@ -4,6 +4,11 @@ public class TextEditor {
     private SpellChecker spellChecker;
     private String name;
 
+    public TextEditor(SpellChecker spellChecker, String name) {
+        this.spellChecker = spellChecker;
+        this.name = name;
+    }
+
     public SpellChecker getSpellChecker() {
         return spellChecker;
     }
