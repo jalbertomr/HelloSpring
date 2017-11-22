65868c5412f311e6d948695086ab72cedb576439
jalbertomr <jalbertomr@yahoo.com>
2017-11-21 19:42:10 -0600

    @Autowired on Properties (TextEditor.java)

--- src/com/tutorialspoint/TextEditor.java
+++ src/com/tutorialspoint/TextEditor.java
@@ -3,13 +3,17 @@ package com.tutorialspoint;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class TextEditor {
+    @Autowired
     private SpellChecker spellChecker;
+
+    public TextEditor() {
+        System.out.println("TextEditor: Dentro del constructor");
+    }
 
     public SpellChecker getSpellChecker() {
         return spellChecker;
     }
 
-    @Autowired
     public void setSpellChecker(SpellChecker spellChecker) {
         System.out.println("TextEditor: Dentro de setSpellChecker");
         this.spellChecker = spellChecker;
