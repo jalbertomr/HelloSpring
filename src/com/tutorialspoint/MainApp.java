b04a904a9888b11216bc1a8914d97cefe4fbf842
jalbertomr <jalbertomr@yahoo.com>
2017-11-21 21:08:17 -0600

    @Configuration & @Bean Annotations (AppConfig.java HelloWorld.java MainApp.java)

--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -2,14 +2,21 @@ package com.tutorialspoint;
 
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 public class MainApp {
     public static void main(String... args) {
-        XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("Beans.xml"));
-
-        HelloWorld o = (HelloWorld) factory.getBean("helloWorld");
-        o.getMessage();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        /*   o tambien
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AppConfig.class,OtraConfig.class);
+        context.register(MasConfig.class);
+        context.refresh();
+        */
+        HelloWorld helloWorld = context.getBean(HelloWorld.class);
+        helloWorld.setMessage("Hola Perrito Mundo!");
+        helloWorld.getMessage();
     }
 }
