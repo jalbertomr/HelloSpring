9d3935ef373314b07bcd0d220b34ba5b1779e324
jalbertomr <jalbertomr@yahoo.com>
2018-05-02 17:29:27 -0500

    DependencyInjectionENSP

---
+++ src/com/tutorialspoint/SpellCheckerEN.java
@@ -0,0 +1,8 @@
+package com.tutorialspoint;
+
+public class SpellCheckerEN extends SpellChecker {
+    @Override
+    public void spellChecking() {
+        System.out.println("SpellCheckerEN: spellChecking()");
+    }
+}
---
+++ src/com/tutorialspoint/SpellCheckerSP.java
@@ -0,0 +1,8 @@
+package com.tutorialspoint;
+
+public class SpellCheckerSP extends SpellChecker {
+    @Override
+    public void spellChecking() {
+        System.out.println("SpellCheckerSP: spellChecking()");
+    }
+}
