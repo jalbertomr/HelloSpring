f267b91182c85783f6a161e1c8f9a7324e8ecf09
jalbertomr <jalbertomr@yahoo.com>
2018-05-03 10:57:36 -0500

    Transaction Management

--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -10,17 +10,22 @@ public class MainApp {
         ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
         StudentJDBCTemplate studentJDBCTemplate = (StudentJDBCTemplate) context.getBean("studentJDBCTemplate");
+        System.out.println("---- Borrando todos los registros -----");
+        studentJDBCTemplate.deleteAll();
+
         System.out.println("---- creando registros -----");
-        studentJDBCTemplate.create("Juan",23);
-        studentJDBCTemplate.create("Lucia", 21);
-        studentJDBCTemplate.create("Fulanita", 34);
+        studentJDBCTemplate.create("Juan",23, 89, 2014);
+        studentJDBCTemplate.create("Lucia", 21, 95, 2017);
+        studentJDBCTemplate.create("Fulanita", 34, 100, 2018);
 
         System.out.println("---- Desplegando multiples registros ----");
-        List<Student> students = studentJDBCTemplate.listStudents();
-        for (Student record: students){
+        List<StudentMarks> studentsMarks = studentJDBCTemplate.listStudentsMarks();
+        for (StudentMarks record: studentsMarks){
             System.out.print("ID: " + record.getId());
             System.out.print(", name: " + record.getName());
             System.out.println(", age: " + record.getAge());
+            System.out.println(", mark: " + record.getMarks());
+            System.out.println(", year: " + record.getYear());
         }
 
         System.out.println("---- desplegando registro con id = 2");
--- src/com/tutorialspoint/StudentJDBCTemplate.java
+++ src/com/tutorialspoint/StudentJDBCTemplate.java
@@ -1,9 +1,13 @@
 package com.tutorialspoint;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import javax.sql.DataSource;
 import java.util.List;
@@ -13,19 +17,43 @@ public class StudentJDBCTemplate implements StudentDAO {
     private DataSource dataSource;
     private JdbcTemplate jdbcTemplateObj;
     private SimpleJdbcCall jdbcCall;
+    private PlatformTransactionManager transactionManager;
 
     @Override
     public void setDataSource(DataSource ds) {
       this.dataSource = ds;
       this.jdbcTemplateObj = new JdbcTemplate(ds);
-      this.jdbcCall = new SimpleJdbcCall(ds).withProcedureName("getRecord");
+     // this.jdbcCall = new SimpleJdbcCall(ds).withProcedureName("getRecord");
+    }
+
+    public void setTransactionManager(PlatformTransactionManager transactionManager){
+        this.transactionManager = transactionManager;
     }
 
     @Override
-    public void create(String name, Integer age) {
-        String SQL = "insert into student (name, age) values (?,?)";
-        jdbcTemplateObj.update( SQL, name, age);
-        System.out.println("registro creado name: " + name + " age: " + age);
+    public void create(String name, Integer age, Integer marks, Integer year) {
+        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+        TransactionStatus status = transactionManager.getTransaction(def);
+
+        try {
+            String SQL1 = "insert into student (name, age) values (?,?)";
+            System.out.println(SQL1);
+            jdbcTemplateObj.update( SQL1, name, age);
+
+            //obten el ultimo id de student para ser usado en la tabla marks
+            String SQL2 = "select max(id) from student";
+            int sid = jdbcTemplateObj.queryForObject(SQL2, int.class);
+
+            String SQL3 = "insert into marks(sid, marks, year) values (?,?,?)";
+            jdbcTemplateObj.update( SQL3, sid, marks, year);
+            System.out.println("registro creado name: " + name + " age: " + age);
+
+            transactionManager.commit(status);
+        }catch(DataAccessException e){
+            System.out.println("Error al crear registro, Rolling Back " + e);
+            transactionManager.rollback(status);
+            throw e;
+        }
         return;
     }
 
@@ -54,11 +82,28 @@ public class StudentJDBCTemplate implements StudentDAO {
         return students;
     }
 
+    public List<StudentMarks> listStudentsMarks(){
+        String SQL = "select * from student s, marks m where s.id=m.sid";
+        List<StudentMarks> studentMarks = jdbcTemplateObj.query(SQL, new StudentMarksMapper());
+        return studentMarks;
+    }
+
     @Override
     public void delete(Integer id) {
         String SQL = "delete from student where id = ?";
         jdbcTemplateObj.update(SQL, id);
         System.out.println("registro Borrado con id: " + id);
+        return;
+    }
+
+    @Override
+    public void deleteAll() {
+        String SQL = "delete from student";
+        int result =jdbcTemplateObj.update(SQL);
+        System.out.println("tabla student eliminados todos los registros result:" + result);
+        SQL = "delete from marks";
+        result = jdbcTemplateObj.update(SQL);
+        System.out.println("tabla marks eliminados todos los registros result: " + result);
         return;
     }
 
